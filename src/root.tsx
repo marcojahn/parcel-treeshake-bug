026efd08d4cb2c9354f00f2b79ef5e1206357683
Marco Jahn <marco.jahn@gmail.com>
2019-09-03 08:31:48 +0200

    possible fix

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
-import { Layout } from "antd";
+
+import { Layout } from "antd"; // NOT WORKING
+// WORKING import Layout from "antd/es/layout";
 
 const { Sider } = Layout;
 
 const Root = () => {
   return (
     <Layout>
-      <Sider>
-        <nav></nav>
-      </Sider>
       <Layout></Layout>
     </Layout>
   );
